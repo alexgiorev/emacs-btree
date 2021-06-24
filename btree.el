2f7781a2165a78ad263c15bbeb720c69b91d5a2e
alex <alex.giorev@gmail.com>
2021-06-24 12:16:56 +0300

    btree--node -> btree-node

--- btree.el
+++ btree.el
@@ -2,7 +2,7 @@
 ;; Tree
 
 (defun btree (cmp min-degree &optional root)
-  (setq root (or root (btree--node)))
+  (setq root (or root (btree-node)))
   ;; root min-degree cmp
   (record 'btree root min-degree cmp))
 
@@ -35,7 +35,7 @@ properties. You can use `btree-check' for that."
         (while (and entries2 (< depth (caar entries2)))
           ;; if we are positioned on a child of NODE...
           (if (= (caar entries2) (1+ depth))
-              (btree--node-append-child node (cdar entries2)))
+              (btree-node-append-child node (cdar entries2)))
           ;; continue with next entry
           (setq entries2 (cdr entries2)))))
     tree))
@@ -58,7 +58,7 @@ function which extracts the keys of the b-tree node from the org node."
               (let* ((components (org-heading-components))
                      (depth (nth 0 components))
                      (keys (funcall keyfunc))
-                     (node (btree--node nil keys nil)))
+                     (node (btree-node nil keys nil)))
                 (push (cons depth node) entries))))
       (org-map-region extract-entry (point-min) (point-max))
       (reverse entries))))
@@ -76,7 +76,7 @@ function which extracts the keys of the b-tree node from the org node."
 
 (defun btree--walk-node (node btree-walk-current-depth)
   (funcall btree-node-fn node)
-  (dolist (child (btree--node-children node))
+  (dolist (child (btree-node-children node))
     (btree--walk-node child (1+ btree-walk-current-depth))))
 
 (defun btree-map-keys (btree-key-fn btree &optional return)
@@ -97,8 +97,8 @@ the number of keys is one less the number of children."
     (reverse result)))
 
 (defun btree--map-node-keys (node)
-  (let ((keys (btree--node-keys node))
-        (children (btree--node-children node)))
+  (let ((keys (btree-node-keys node))
+        (children (btree-node-children node)))
     (if (not children)
         (mapcar btree-key-fn keys)
       (while keys
@@ -123,7 +123,7 @@ the number of keys is one less the number of children."
     (btree-depth-first-walk 'btree--check-leaf-depth btree t)))
 
 (defun btree--check-leaf-depth (node)
-  (if (btree--node-leafp node)
+  (if (btree-node-leafp node)
       (if leaf-depth
           ;; this is not the first leaf
           (if (/= leaf-depth btree-walk-current-depth)
@@ -143,13 +143,13 @@ number of children."
 (defun btree--check-node-degree (node)
   "Assumes that when called `min-degree' and `max-degree' are set according to
 the btree which is being traversed."
-  (let ((min-degree (if (not (btree--node-parent node)) 1 min-degree))
-        (degree (length (btree--node-children node)))
-        (key-count (length (btree--node-keys node))))
-    (unless (or (and (btree--node-leafp node)
+  (let ((min-degree (if (not (btree-node-parent node)) 1 min-degree))
+        (degree (length (btree-node-children node)))
+        (key-count (length (btree-node-keys node))))
+    (unless (or (and (btree-node-leafp node)
                      (<= 1 key-count (1- max-degree)))
                 (and (<= min-degree degree max-degree)
-                     (= (length (btree--node-keys node)) (1- degree))))
+                     (= (length (btree-node-keys node)) (1- degree))))
       (throw 'btree-end-walk nil))))
 
 (defun btree--check-order (btree)
@@ -196,35 +196,35 @@ those two keys."
   (let* ((cmp (btree-cmp btree))
          (node (btree--find-leaf btree key))
          parent)
-    (btree--node-insert-key node key)
-    (while (when (btree--node-overflowing-p node)
-             (setq parent (btree--node-parent node))
-             (btree--node-split node)
+    (btree-node-insert-key node key)
+    (while (when (btree-node-overflowing-p node)
+             (setq parent (btree-node-parent node))
+             (btree-node-split node)
              (when parent (setq node parent))))))
 
 (defun btree-insert-org (btree &optional node-to-org)
   "Inserts into the current buffer at point an org-mode representation of
   BTREE. NODE-TO-ORG is a function which transforms a B-tree node into an
   org-mode node, but it doesn't do anything for the children."
-  (setq node-to-org (or node-to-org btree--node-to-org))
+  (setq node-to-org (or node-to-org btree-node-to-org))
   (btree-depth-first-walk 'btree--insert-org-node btree))
 
 (defun btree--insert-org-node (node)
   (insert (funcall node-to-org node (1+ btree-walk-current-depth))))
 
-(defun btree--node-to-org-print (node level)
+(defun btree-node-to-org-print (node level)
   (concat (make-string level ?*)
           (format " %s\n"
-                  (btree--node-keys node))))
+                  (btree-node-keys node))))
 
-(defvar btree--node-to-org 'btree--node-to-org-print
+(defvar btree-node-to-org 'btree-node-to-org-print
   "The default function which converts a B-tree node to an org-mode entry")
 
 (defun btree--find-leaf (btree key)
   "Finds the leftmost leaf in BTREE into which KEY belongs. If KEY is in some
 leaf, this will return the leftmost such leaf. Assumes `cmp' is set."
   (let ((node (btree-root btree)))
-    (while (not (btree--node-leafp node))
+    (while (not (btree-node-leafp node))
       (setq node (btree--continuation-child node key)))
     node))
 
@@ -240,9 +240,9 @@ leaf, this will return the leftmost such leaf. Assumes `cmp' is set."
   "Returns the child of NODE which we should visit to continue the search for
 KEY. Assumes that at the time of call `cmp' stores the key comparison
 function. If NODE is a leaf, returns `nil'"
-  (unless (btree--node-leafp node)
-    (let ((keys (btree--node-keys node))
-          (children (btree--node-children node)))
+  (unless (btree-node-leafp node)
+    (let ((keys (btree-node-keys node))
+          (children (btree-node-children node)))
       (while (and keys (eq (funcall cmp (car keys) key)'<))
         (setq keys (cdr keys)
               children (cdr children)))
@@ -253,53 +253,53 @@ function. If NODE is a leaf, returns `nil'"
 ;; in which the node finds itself in. This is sensible because normally we call
 ;; a node function in the context of some tree.
 
-(defun btree--node (&optional parent keys children)
+(defun btree-node (&optional parent keys children)
   (record 'btree-node parent keys children))
 
-(defun btree--node-parent (node)
+(defun btree-node-parent (node)
   (aref node 1))
 
-(defun btree--node-set-parent (node parent)
+(defun btree-node-set-parent (node parent)
   (aset node 1 parent))
 
-(defun btree--node-keys (node)
+(defun btree-node-keys (node)
   (aref node 2))
 
-(defun btree--node-set-keys (node keys)
+(defun btree-node-set-keys (node keys)
   (aset node 2 keys))
 
-(defun btree--node-children (node)
+(defun btree-node-children (node)
   (aref node 3))
 
-(defun btree--node-set-children (node children)
+(defun btree-node-set-children (node children)
   (aset node 3 children))
 
-(defun btree--node-append-child (node child)
-  (let ((children (btree--node-children node)))
+(defun btree-node-append-child (node child)
+  (let ((children (btree-node-children node)))
     (if children
         (nconc children (list child))
-      (btree--node-set-children node (list child)))
-    (btree--node-set-parent child node)))
+      (btree-node-set-children node (list child)))
+    (btree-node-set-parent child node)))
 
-(defun btree--node-leafp (node)
-  (not (btree--node-children node)))
+(defun btree-node-leafp (node)
+  (not (btree-node-children node)))
 
-(defun btree--node-depth (node)
+(defun btree-node-depth (node)
   (let ((depth -1))
     (while node
       (setq depth (1+ depth))
-      (setq node (btree--node-parent node)))
+      (setq node (btree-node-parent node)))
     depth))
 
-(defun btree--node-insert-key (node key)
+(defun btree-node-insert-key (node key)
   "Inserts KEY into NODE's keys in the correct position.
 Assumes that `cmp' is set."
-  (let ((keys (btree--node-keys node))
+  (let ((keys (btree-node-keys node))
         next-key)
     (if (not keys)
-        (btree--node-set-keys node (list key))
+        (btree-node-set-keys node (list key))
       (if (memq (funcall cmp key (car keys)) '(< =))
-          (btree--node-set-keys node (cons key keys))
+          (btree-node-set-keys node (cons key keys))
         ;; at the end of this loop, `keys' will be the cell after which we
         ;; should insert KEY
         (while (and (setq next-key (cadr keys))
@@ -307,40 +307,40 @@ Assumes that `cmp' is set."
           (setq keys (cdr keys)))
         (setcdr keys (cons key (cdr keys)))))))
 
-(defun btree--node-overflowing-p (node)
-  (>= (length (btree--node-keys node))
+(defun btree-node-overflowing-p (node)
+  (>= (length (btree-node-keys node))
      (btree-max-degree btree)))
 
-(defun btree--node-split (node)
-  (let* ((parent (btree--node-parent node))
-         (split-keys (btree--node-split-keys (btree--node-keys node)))
+(defun btree-node-split (node)
+  (let* ((parent (btree-node-parent node))
+         (split-keys (btree-node-split-keys (btree-node-keys node)))
          (left-keys (car split-keys)) (right-keys (caddr split-keys))
          (parent-key (cadr split-keys))
-         (split-children (btree--node-split-children
-                          (btree--node-children node)))
+         (split-children (btree-node-split-children
+                          (btree-node-children node)))
          (left-children (car split-children))
          (right-children (cadr split-children))
-         (left-node (btree--node nil left-keys left-children))
-         (right-node (btree--node nil right-keys right-children)))
+         (left-node (btree-node nil left-keys left-children))
+         (right-node (btree-node nil right-keys right-children)))
     (dolist (child left-children)
-      (btree--node-set-parent child left-node))
+      (btree-node-set-parent child left-node))
     (dolist (child right-children)
-      (btree--node-set-parent child right-node))
+      (btree-node-set-parent child right-node))
     ;; insert key and new children into parent. If the parent is a new node, set
     ;; it up as the root of the tree
     (if (not parent)
         (progn
           (setq parent
-                (btree--node
+                (btree-node
                  nil (list parent-key) (list left-node right-node)))
           (btree-set-root btree parent))
-      (btree--node-insert-key parent parent-key)
-      (btree--node-split-replace-child parent node left-node right-node))
+      (btree-node-insert-key parent parent-key)
+      (btree-node-split-replace-child parent node left-node right-node))
     ;; set parent of new children
-    (btree--node-set-parent left-node parent)
-    (btree--node-set-parent right-node parent)))
+    (btree-node-set-parent left-node parent)
+    (btree-node-set-parent right-node parent)))
 
-(defun btree--node-split-keys (keys)
+(defun btree-node-split-keys (keys)
   "Returns a list that represents the triple (left-child-keys parent-key
 right-child-keys). Then KEYS has an even length, the left child will have one
 more key than the right child."
@@ -350,7 +350,7 @@ more key than the right child."
          (right-split (last keys split-index)))
     (list left-split (car right-split) (cdr right-split))))
 
-(defun btree--node-split-children (children)
+(defun btree-node-split-children (children)
   "Returns a list representing the pair (left-children right-children). When
 CHILDREN has an odd length, the left list will have one more child than the
 right one"
@@ -358,12 +358,12 @@ right one"
     (list (butlast children split-point) (last children split-point))))
          
 
-(defun btree--node-split-replace-child (node child left right)
+(defun btree-node-split-replace-child (node child left right)
   "Assumes that CHILD is a child of NODE. Removes CHILD from the children list
 and inserts LEFT and RIGHT in its place."
-  (let ((children (btree--node-children node)))
+  (let ((children (btree-node-children node)))
     (if (eq child (car children))
-        (btree--node-set-children
+        (btree-node-set-children
          node (cons left (cons right (cdr children))))
       (while (not (eq (cadr children) child))
         (setq children (cdr children)))
@@ -388,7 +388,7 @@ access the second child, then its third child, then its fifth child, you call
 (baddr btree 1 2 4)"
   (let ((node (btree-root btree)))
     (dolist (index indexes)
-      (setq node (nth index (btree--node-children node))))
+      (setq node (nth index (btree-node-children node))))
     node))
 
 (defun btree-from-random-sequence (low high &optional cmp min-degree)
